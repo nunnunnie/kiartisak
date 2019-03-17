4f818726352845b8f0dc43b97c82d085a697051b
Kedsinee Junraksa <nunnunnie@gmail.com>
2019-03-17 11:57:10 +0700

    edit 1

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+ /* items = ["Nun","NUn1","Nun2","Nun3","Nun4","Nun5"]; */
 
+  items = [
+    { name : "Nun", tel  : "0877481760", avatar : "1.jpg"},
+    { name : "Nun1", tel : "0877481760", avatar : "2.jpg"},
+    { name : "Nun2", tel : "0877481760", avatar : "3.jpg"},
+    { name : "Nun3", tel : "0877481760", avatar : "4.jpg"},
+    { name : "Nun4", tel : "0877481760", avatar : "5.jpg"}
+  ];
+itemSelected(item : string){
+  console.log("Selected Item", item);
+}
   constructor(public navCtrl: NavController) {
 
   }
